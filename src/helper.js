10b2cb01694dcef9f15fac1b4bf9028601cfa0cb
Tresor11 <tresor.moise2001@gmail.com>
2020-03-29 10:25:01 +0200

    fixed the package.jon file

--- src/helper.js
+++ src/helper.js
@@ -9,7 +9,7 @@ function currentDate() {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   const days = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
   const d = new Date();
-  return `${days[d.getDay()]}, ${d.getDate()} ${months[d.getMonth()]}`;
+  return `${days[d.getDay() - 1]}, ${d.getDate()} ${months[d.getMonth()]}`;
 }
 
 export { background, currentDate };
